47b76e0225769a57b868006f90d310c9ffcd5e74
enginbzky <enginbozkaya1@gmail.com>
2022-11-24 16:48:48 +0100

    The errors fixed

--- functions.js
+++ functions.js
@@ -1,5 +1,38 @@
-const changeEmailFromGmailToHicoders = () => {};
+// const list = [
+//   "Switzerland55@gmail.com",
+//   "Zurich4155@gmail.com",
+//   "HiCoders41@gmail.com",
+// ];
+
+const changeEmailFromGmailToHicoders = (pStringArray) => {
+  let HiCoders = "hicoders.ch";
+  const orderOfEmails = pStringArray.map((abbas) =>
+    abbas
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("")
+      .slice(9)
+      .split("")
+      .reverse()
+      .join("")
+      .concat(HiCoders)
+  );
+  const result = orderOfEmails.reverse();
+  return result;
+};
 
 const findSuperNumbers = () => {};
 
+// let numberss = [455, 789, 153];
+
+// const findSuperNumbers = (pNumberArray) => {
+//   const result = pNumberArray.map((element) => element.toString()).find(());
+//   console.log(result);
+// };
+
+// findSuperNumbers(numberss);
+
 export { changeEmailFromGmailToHicoders, findSuperNumbers };
+
+// element[0] ** (1 / 3) + element[1] ** (1 / 3) + element[2] ** (1 / 3);
